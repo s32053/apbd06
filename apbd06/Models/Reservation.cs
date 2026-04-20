d80acfd2a90bce7833093f464b365be073130a91
s32053 <s32053@pjwstk.edu.pl>
2026-04-20 20:54:19 +0200

    Models

---
+++ apbd06/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd06.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    [Required]
+    public int RoomId { get; set; }
+    [Required]
+    public string OrganizerName { get; set; } = string.Empty;
+    [Required]
+    public string Topic { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public string Status { get; set; } = "planned";
+}
